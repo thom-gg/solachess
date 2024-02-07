7d3ef4a4498a32e8b4960a18bbe69220b760cd70
Thom <79939935+thom-gg@users.noreply.github.com>
2024-02-07 18:21:52 +0100

    Head title and favicon

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -1,12 +1,16 @@
 import type { AppProps } from "next/app"
 import WalletContextProvider from "../components/WalletContextProvider"
 import { ChakraProvider } from "@chakra-ui/react"
+import Head from "next/head"
 
 
 function MyApp({ Component, pageProps }: AppProps) {
     return (
         <ChakraProvider>
             <WalletContextProvider>
+                <Head>
+                    <title>Solachess</title>
+                </Head>
                 <Component {...pageProps} />
             </WalletContextProvider>
         </ChakraProvider>
